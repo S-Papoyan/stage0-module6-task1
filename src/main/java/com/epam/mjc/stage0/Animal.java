e40ed649b7c60fd685baf1c710dbbaa2760d77a4
Sergey <spapoyan1987@gmail.com>
2022-12-20 01:39:18 +0400

    commit

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,26 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+
+    public String getDescription() {
+        Animal animal = new Animal(color, numberOfPaws, hasFur);
+        String s = !animal.hasFur ? "no" : "a";
+        if (animal.numberOfPaws > 1) {
+            return "This animal is mostly " + animal.color + ". It has " + animal.numberOfPaws + " paws and " +
+                    s + " fur.";
+        }
+        return "This animal is mostly " + animal.color + ". It has " + animal.numberOfPaws + " paw and " +
+                s + " fur.";
+    }
 }
